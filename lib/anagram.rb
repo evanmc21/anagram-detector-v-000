fa88543e3f7d8139070434cfead632308006cc39
Evan Candler <emcandler21@gmail.com>
2018-02-28 23:31:45 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,6 +7,13 @@ class Anagram
     @beyonce = beyonce
   end
 
-  def match
+  def match(array)
+    array.select {|beyonce| beyonce.split("").sort == @beyonce.split("").sort}
   end 
 end
+
+
+#splits each into letters and sorts letters alphabetically
+#splits beyonce into single letters and sorts them alphabetically
+#iterates over the array of alphabetized letters and verifys that they match @beyonce's letters
+#returns the orginal beyonce from array which is anagram of @beyonce
